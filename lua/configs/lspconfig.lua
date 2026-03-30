21ed92438923731cd9fdaa7d6ba8ca9d28ab383e
asyncPranav <asyncpranav@gmail.com>
2026-03-30 13:39:35 +0530

    remove vtsls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "vtsls", "pyright", "emmet_ls", "tailwindcss" }
+local servers = { "html", "cssls", "typescript-language-server", "pyright", "emmet_ls", "tailwindcss" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
